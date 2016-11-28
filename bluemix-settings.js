c7d9c7b35e92880566914f1df736b6ea80c2dd3e
rajeevkumarp <pingrajeev@gmail.com>
2016-11-27 22:04:01 -0500

    Update bluemix-settings.js

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -51,7 +51,10 @@ var settings = module.exports = {
     // Serve up the welcome page
     httpStatic: path.join(__dirname,"public"),
 
-    functionGlobalContext: { },
+    functionGlobalContext: {
+        
+    
+    },
 
     storageModule: require("./couchstorage")
 }
